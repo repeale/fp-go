2cb2095b4ababe19a51ec9f6d52d61c4ec69a5f3
Force4760 <gforce4760@gmail.com>
2022-08-08 15:59:23 +0100

    Add testing to Pipe and Compose

--- compose_test.go
+++ compose_test.go
@@ -40,3 +40,132 @@ func TestCompose4_Example(t *testing.T) {
 		t.Error("Should perform right-to-left function composition of two functions. Received:", res)
 	}
 }
+
+/* Generated with the following python code
+def compose_test(n: int) -> str:
+    fns = ", ".join(f"fn{i % 2}" for i in range(n))
+
+	expect = 0
+	for i in range(n-1, -1, -1):
+		if i % 2 == 0:
+			expect += 1
+		else:
+			expect *= 2
+
+    return f"""func TestCompose{n}_Example(t *testing.T) {{
+    fn0 := func(x int) int {{ return x + 1 }}
+	fn1 := func(x int) int {{ return x * 2 }}
+    res := Compose{n}({fns})(0)
+    if res != {expect} {{
+        t.Error("Should perform right-to-left function composition of {n} functions. Received:", res)
+    }}
+}}"""
+*/
+
+func TestCompose5_Example(t *testing.T) {
+	fn0 := func(x int) int { return x + 1 }
+	fn1 := func(x int) int { return x * 2 }
+	res := Compose5(fn0, fn1, fn0, fn1, fn0)(0)
+	if res != 7 {
+		t.Error("Should perform right-to-left function composition of 5 functions. Received:", res)
+	}
+}
+
+func TestCompose6_Example(t *testing.T) {
+	fn0 := func(x int) int { return x + 1 }
+	fn1 := func(x int) int { return x * 2 }
+	res := Compose6(fn0, fn1, fn0, fn1, fn0, fn1)(0)
+	if res != 7 {
+		t.Error("Should perform right-to-left function composition of 6 functions. Received:", res)
+	}
+}
+
+func TestCompose7_Example(t *testing.T) {
+	fn0 := func(x int) int { return x + 1 }
+	fn1 := func(x int) int { return x * 2 }
+	res := Compose7(fn0, fn1, fn0, fn1, fn0, fn1, fn0)(0)
+	if res != 15 {
+		t.Error("Should perform right-to-left function composition of 7 functions. Received:", res)
+	}
+}
+
+func TestCompose8_Example(t *testing.T) {
+	fn0 := func(x int) int { return x + 1 }
+	fn1 := func(x int) int { return x * 2 }
+	res := Compose8(fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1)(0)
+	if res != 15 {
+		t.Error("Should perform right-to-left function composition of 8 functions. Received:", res)
+	}
+}
+
+func TestCompose9_Example(t *testing.T) {
+	fn0 := func(x int) int { return x + 1 }
+	fn1 := func(x int) int { return x * 2 }
+	res := Compose9(fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0)(0)
+	if res != 31 {
+		t.Error("Should perform right-to-left function composition of 9 functions. Received:", res)
+	}
+}
+
+func TestCompose10_Example(t *testing.T) {
+	fn0 := func(x int) int { return x + 1 }
+	fn1 := func(x int) int { return x * 2 }
+	res := Compose10(fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1)(0)
+	if res != 31 {
+		t.Error("Should perform right-to-left function composition of 10 functions. Received:", res)
+	}
+}
+
+func TestCompose11_Example(t *testing.T) {
+	fn0 := func(x int) int { return x + 1 }
+	fn1 := func(x int) int { return x * 2 }
+	res := Compose11(fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0)(0)
+	if res != 63 {
+		t.Error("Should perform right-to-left function composition of 11 functions. Received:", res)
+	}
+}
+
+func TestCompose12_Example(t *testing.T) {
+	fn0 := func(x int) int { return x + 1 }
+	fn1 := func(x int) int { return x * 2 }
+	res := Compose12(fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1)(0)
+	if res != 63 {
+		t.Error("Should perform right-to-left function composition of 12 functions. Received:", res)
+	}
+}
+
+func TestCompose13_Example(t *testing.T) {
+	fn0 := func(x int) int { return x + 1 }
+	fn1 := func(x int) int { return x * 2 }
+	res := Compose13(fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0)(0)
+	if res != 127 {
+		t.Error("Should perform right-to-left function composition of 13 functions. Received:", res)
+	}
+}
+
+func TestCompose14_Example(t *testing.T) {
+	fn0 := func(x int) int { return x + 1 }
+	fn1 := func(x int) int { return x * 2 }
+	res := Compose14(fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1)(0)
+	if res != 127 {
+		t.Error("Should perform right-to-left function composition of 14 functions. Received:", res)
+	}
+}
+
+func TestCompose15_Example(t *testing.T) {
+	fn0 := func(x int) int { return x + 1 }
+	fn1 := func(x int) int { return x * 2 }
+	res := Compose15(fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0)(0)
+	if res != 255 {
+		t.Error("Should perform right-to-left function composition of 15 functions. Received:", res)
+	}
+}
+
+func TestCompose16_Example(t *testing.T) {
+	fn0 := func(x int) int { return x + 1 }
+	fn1 := func(x int) int { return x * 2 }
+	res := Compose16(fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1)(0)
+	if res != 255 {
+		t.Error("Should perform right-to-left function composition of 16 functions. Received:", res)
+	}
+}
--- pipe_test.go
+++ pipe_test.go
@@ -40,3 +40,131 @@ func TestPipe4_Example(t *testing.T) {
 		t.Error("Should perform left-to-right function composition of two functions. Received:", res)
 	}
 }
+
+/* Generated with the following python code
+def pipe_test(n: int) -> str:
+	fns = ", ".join(f"fn{i % 2}" for i in range(n))
+
+	expect = 0
+	for i in range(n):
+		if i % 2 == 0:
+			expect += 1
+		else:
+			expect *= 2
+
+    return f"""func TestPipe{n}_Example(t *testing.T) {{
+    fn0 := func(x int) int {{ return x + 1 }}
+	fn1 := func(x int) int {{ return x * 2 }}
+    res := Pipe{n}({fns})(0)
+    if res != {expect} {{
+        t.Error("Should perform left-to-right function composition of {n} functions. Received:", res)
+    }}
+}}"""
+*/
+func TestPipe5_Example(t *testing.T) {
+	fn0 := func(x int) int { return x + 1 }
+	fn1 := func(x int) int { return x * 2 }
+	res := Pipe5(fn0, fn1, fn0, fn1, fn0)(0)
+	if res != 7 {
+		t.Error("Should perform left-to-right function composition of 5 functions. Received:", res)
+	}
+}
+
+func TestPipe6_Example(t *testing.T) {
+	fn0 := func(x int) int { return x + 1 }
+	fn1 := func(x int) int { return x * 2 }
+	res := Pipe6(fn0, fn1, fn0, fn1, fn0, fn1)(0)
+	if res != 14 {
+		t.Error("Should perform left-to-right function composition of 6 functions. Received:", res)
+	}
+}
+
+func TestPipe7_Example(t *testing.T) {
+	fn0 := func(x int) int { return x + 1 }
+	fn1 := func(x int) int { return x * 2 }
+	res := Pipe7(fn0, fn1, fn0, fn1, fn0, fn1, fn0)(0)
+	if res != 15 {
+		t.Error("Should perform left-to-right function composition of 7 functions. Received:", res)
+	}
+}
+
+func TestPipe8_Example(t *testing.T) {
+	fn0 := func(x int) int { return x + 1 }
+	fn1 := func(x int) int { return x * 2 }
+	res := Pipe8(fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1)(0)
+	if res != 30 {
+		t.Error("Should perform left-to-right function composition of 8 functions. Received:", res)
+	}
+}
+
+func TestPipe9_Example(t *testing.T) {
+	fn0 := func(x int) int { return x + 1 }
+	fn1 := func(x int) int { return x * 2 }
+	res := Pipe9(fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0)(0)
+	if res != 31 {
+		t.Error("Should perform left-to-right function composition of 9 functions. Received:", res)
+	}
+}
+
+func TestPipe10_Example(t *testing.T) {
+	fn0 := func(x int) int { return x + 1 }
+	fn1 := func(x int) int { return x * 2 }
+	res := Pipe10(fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1)(0)
+	if res != 62 {
+		t.Error("Should perform left-to-right function composition of 10 functions. Received:", res)
+	}
+}
+
+func TestPipe11_Example(t *testing.T) {
+	fn0 := func(x int) int { return x + 1 }
+	fn1 := func(x int) int { return x * 2 }
+	res := Pipe11(fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0)(0)
+	if res != 63 {
+		t.Error("Should perform left-to-right function composition of 11 functions. Received:", res)
+	}
+}
+
+func TestPipe12_Example(t *testing.T) {
+	fn0 := func(x int) int { return x + 1 }
+	fn1 := func(x int) int { return x * 2 }
+	res := Pipe12(fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1)(0)
+	if res != 126 {
+		t.Error("Should perform left-to-right function composition of 12 functions. Received:", res)
+	}
+}
+
+func TestPipe13_Example(t *testing.T) {
+	fn0 := func(x int) int { return x + 1 }
+	fn1 := func(x int) int { return x * 2 }
+	res := Pipe13(fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0)(0)
+	if res != 127 {
+		t.Error("Should perform left-to-right function composition of 13 functions. Received:", res)
+	}
+}
+
+func TestPipe14_Example(t *testing.T) {
+	fn0 := func(x int) int { return x + 1 }
+	fn1 := func(x int) int { return x * 2 }
+	res := Pipe14(fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1)(0)
+	if res != 254 {
+		t.Error("Should perform left-to-right function composition of 14 functions. Received:", res)
+	}
+}
+
+func TestPipe15_Example(t *testing.T) {
+	fn0 := func(x int) int { return x + 1 }
+	fn1 := func(x int) int { return x * 2 }
+	res := Pipe15(fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0)(0)
+	if res != 255 {
+		t.Error("Should perform left-to-right function composition of 15 functions. Received:", res)
+	}
+}
+
+func TestPipe16_Example(t *testing.T) {
+	fn0 := func(x int) int { return x + 1 }
+	fn1 := func(x int) int { return x * 2 }
+	res := Pipe16(fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1, fn0, fn1)(0)
+	if res != 510 {
+		t.Error("Should perform left-to-right function composition of 16 functions. Received:", res)
+	}
+}
