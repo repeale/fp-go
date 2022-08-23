9dda2c78ac759c2ee628c4cbee63838f11f37202
Force4760 <gforce4760@gmail.com>
2022-08-23 09:32:24 +0100

    Remove comments and move to generators

--- generators/generators.py
+++ generators/generators.py
@@ -36,3 +36,29 @@ func TestPipe{n}_Example(t *testing.T) {{
 		t.Error("Should perform left-to-right function composition of {n} functions. Received:", res)
 	}}
 }}"""
+
+
+def pipe_func(n: int) -> str:
+    gen = "[" + ", ".join(f"T{i+1}" for i in range(n)) + ", R any]"
+    fns = ", ".join(f"fn{i} func(T{i}) T{i+1}" for i in range(1, n)) + f", fn{n} func(T{n}) R"
+    cal = "".join(f"fn{i}(" for i in range(n, 0, -1)) + "t1" + ")" * n
+
+    return f"""// Performs left-to-right function composition of {n} functions
+func Pipe{n}{gen}({fns}) func(T1) R  {{
+    return func(t1 T1) R {{
+        return {cal}
+    }}
+}}"""
+
+
+def compose_func(n: int) -> str:
+    gen = "[" + ", ".join(f"T{i+1}" for i in range(n)) + ", R any]"
+    fns = f"fn{n} func(T{n}) R, " + ", ".join(f"fn{i} func(T{i}) T{i+1}" for i in range(n-1, 0, -1))
+    cal = "".join(f"fn{i}(" for i in range(n, 0, -1)) + "t1" + ")" * n
+
+    return f"""// Performs right-to-left function composition of {n} functions
+func Compose{n}{gen}({fns}) func(T1) R  {{
+    return func(t1 T1) R {{
+        return {cal}
+    }}
+}}"""
