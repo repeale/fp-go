7fb94fa6e4b0dc1c5dc692b8408836ef69815360
Force4760 <gforce4760@gmail.com>
2022-08-25 20:46:41 +0100

    feat: more variations to Curry (#15)
    * Add tests and curry implementation 4-16
    
    * Add generator
    
    * Add mention in the ReadMe

--- generators/generators.py
+++ generators/generators.py
@@ -38,6 +38,27 @@ func TestPipe{n}_Example(t *testing.T) {{
 }}"""
 
 
+def curry_test(n: int) -> str:
+    vars = [f"t{i+1}" for i in range(n)]
+
+    params = ", ".join(vars)
+    result = " + ".join(vars)
+    call   = "".join(f"({i})" for i in range(1, n+1))
+    res    = sum(range(1, n+1))
+
+    return f"""
+func TestCurry{n}_Example(t *testing.T) {{
+    function := func({params} int) int {{ return {result} }}
+    curriedF := Curry{n}(function)
+
+    res := curriedF{call}
+
+    if res != {res} {{
+        t.Error("Should allow to curry a sum. Received:", res)
+    }}
+}}"""
+
+
 def pipe_func(n: int) -> str:
     gen = "[" + ", ".join(f"T{i+1}" for i in range(n)) + ", R any]"
     fns = ", ".join(f"fn{i} func(T{i}) T{i+1}" for i in range(1, n)) + f", fn{n} func(T{n}) R"
@@ -62,3 +83,24 @@ func Compose{n}{gen}({fns}) func(T1) R  {{
         return {cal}
     }}
 }}"""
+
+
+def curry_func(n: int) -> str:
+    gen = ", ".join(f"T{i+1}" for i in range(n))
+    ret = " ".join(f"func(T{i+1})" for i in range(n)) + " R"
+
+    line = lambda x: x * "\t" + f"return func(t{x} T{x}) " + " ".join(f"func(T{i+1})" for i in range(x, n)) + " R {"
+
+    all_returns = "\n".join(line(i+1) for i in range(n))
+
+    function = (n+1) * "\t" + "return fn(" + ", ".join(f"t{i+1}" for i in range(n)) + ")"
+
+    all_closes = "\n".join(i * "\t" + "}" for i in range(n, 0, -1))
+
+    return f"""// Allow to transform a function that receives {n} params in a sequence of unary functions
+func Curry{n}[{gen}, R any](fn func({gen}) R) {ret} {{
+{all_returns}
+{function}
+{all_closes}
+}}
+"""
