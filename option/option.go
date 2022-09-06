77809e1391cb1cb93471d0110dc8d3f3388f1c1a
Alessio Enrico Repetti <repeale@gmail.com>
2022-09-06 22:42:11 +0200

    feat: new constructors for options and functions

--- option/option.go
+++ option/option.go
@@ -42,6 +42,64 @@ func Chain[A, B any](fn func(a A) Option[B]) func(Option[A]) Option[B] {
 	}
 }
 
+// Returns `false` if `None` or returns the boolean result of the application of the given predicate to the `Some` value
+func Exists[T any](predicate func(value T) bool) func(Option[T]) bool {
+	return func(o Option[T]) bool {
+
+		if IsNone(o) {
+			return false
+		}
+
+		return predicate(o.Value)
+	}
+}
+
+// Removes one level of nesting. Returns its bound argument into the outer level.
+func Flatten[T any](o Option[Option[T]]) Option[T] {
+
+	if IsNone(o) {
+		return None[T]()
+	}
+
+	return o.Value
+}
+
+// Constructor of Option from any couple of mutually exclusive `value` and `error`. Returns a None in case we have an error, Some if we have a value and error is nil.
+func FromError[R any](value R, e error) Option[R] {
+
+	if e != nil {
+		return None[R]()
+	}
+
+	return Some(value)
+}
+
+// Constructor of Option from any lazy function that returns a couple of mutually exclusive `value` and `error`. Returns a None in case we have a return error, Some if we have return value and error is nil.
+func FromErrorFn[R any](fn func() (value R, e error)) Option[R] {
+
+	val, err := fn()
+
+	if err != nil {
+		return None[R]()
+	}
+
+	return Some(val)
+}
+
+// Constructor of Option from a predicate.
+// Returns a None if the predicate function over the value return false.
+// Returns a Some if the predicate function over the value return true.
+func FromPredicate[T any](predicate func(value T) bool) func(T) Option[T] {
+	return func(value T) Option[T] {
+
+		if predicate(value) {
+			return Some(value)
+		}
+
+		return None[T]()
+	}
+}
+
 // Extracts the value out of the Option, if it exists. Otherwise returns the function with a default value
 func GetOrElse[T any](onNone fp.Lazy[T]) func(Option[T]) T {
 	return func(o Option[T]) T {
